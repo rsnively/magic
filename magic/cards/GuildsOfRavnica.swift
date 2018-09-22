4241fef87cb4d5125859e80f18f7af5e76799c8e
rory <rory@brainiumstudios.com>
2018-09-22 10:58:09 -0700

    GRN Cage the Culprit

--- magic/cards/GuildsOfRavnica.swift
+++ magic/cards/GuildsOfRavnica.swift
@@ -23,5 +23,18 @@ enum GRN {
         citywideBust.setFlavorText("\"Oh, you fellas are going to love the lockup. Excellent gruel. Very low ceilings.\"\n--Libuse, Boros sergeant")
         return citywideBust
     }
+    static func CageTheCulprit() -> Card {
+        let cageTheCulprit = Card(name: "Cage the Culprit", rarity: .Common, set: set, number: 5)
+        cageTheCulprit.setManaCost("3W")
+        cageTheCulprit.setType(.Instant)
+        cageTheCulprit.addEffect(TargetedEffect(
+            targetingRestriction: { potentialTarget in
+                return potentialTarget.isType(.Creature) && potentialTarget.getToughness() >= 4
+        }, { source, target in
+                target.destroy()
+        }))
+        cageTheCulprit.setFlavorText("\"Reports of Gruul rioters in four districts. Start with the big ones and work your way up.\"\n--Libuse, Boros sergeant")
+        return cageTheCulprit
+    }
     
 }
