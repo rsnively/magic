b23f5dd9827bb55c989725faf24269a98b5a926c
rory <rory@brainiumstudios.com>
2018-12-18 16:14:44 -0800

    VERY important test cards new targeting

--- magic/cards/TestCards.swift
+++ magic/cards/TestCards.swift
@@ -1,18 +1,18 @@
-//import Foundation
-//
-//enum TEST {
-//    static var set = "test"
-//
-//    static func Peek() -> Card {
-//        let peek = Card(name: "Peek", rarity: .Common, set: set, number: 1)
-//        peek.setManaCost("U")
-//        peek.setType(.Instant)
-//        peek.addEffect(TargetedEffect.SinglePlayer(
-//            restriction: TargetedEffect.AnyPlayer,
-//            effect: {
-//                $0.revealHandTo(peek.getController())
-//                peek.getController().drawCard()
-//        }))
-//        return peek
-//    }
-//}
+import Foundation
+
+enum TEST {
+    static var set = "test"
+
+    static func Peek() -> Card {
+        let peek = Card(name: "Peek", rarity: .Common, set: set, number: 1)
+        peek.setManaCost("U")
+        peek.setType(.Instant)
+        peek.addEffect(TargetedEffect.SinglePlayer(
+            restriction: TargetingRestriction.TargetPlayer(),
+            effect: {
+                $0.revealHandTo(peek.getController())
+                peek.getController().drawCard()
+        }))
+        return peek
+    }
+}
